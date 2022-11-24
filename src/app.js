d024a055545b88022f7e5c1f6bb5308fe69eb8f7
Hoon Yim <yimsh4507@gmail.com>
2022-11-23 20:17:33 -0500

    Created app.js

---
+++ src/app.js
@@ -0,0 +1,11 @@
+// Modules
+import React from "react";
+
+// CSS
+import "bootstrap/dist/css/bootstrap.min.css"
+
+export default function App() {
+    return (
+        <div></div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import App from "./app";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
