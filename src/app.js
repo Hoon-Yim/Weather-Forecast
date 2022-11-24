cb4767c3dd61abd76811c60b36b8c0b14c5b720c
Hoon Yim <yimsh4507@gmail.com>
2022-11-24 12:23:13 -0500

    Updated weather.js - searching weather by city function implemented

--- src/app.js
+++ src/app.js
@@ -7,9 +7,22 @@ import "bootstrap/dist/css/bootstrap.min.css"
 // Pages
 import Weather from "./pages/weather";
 
+// Resources
+import background from "./resource/img/cristina-gottardi-CSpjU6hYo_0-unsplash.jpg"
+
 export default function App() {
     return (
-        <div className="d-flex justify-content-center align-items-center" style={{ height: "80vh" }} >
+        <div 
+            className="d-flex justify-content-center align-items-center" 
+            style={{
+                backgroundImage: `url(${background})`,
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+                width: '100vw',
+                height: '100vh'
+            }}
+        >
             <Weather />
         </div>
     );
--- src/pages/weather.js
+++ src/pages/weather.js
@@ -7,10 +7,16 @@ import {
     Card,
     Col,
     Container,
+    Form,
+    FloatingLabel,
     Image,
-    Row
+    Row,
+    Alert
 } from "react-bootstrap";
-import { FaMapMarkerAlt } from "react-icons/fa";
+import { 
+    FaSearch,
+    FaMapMarkerAlt
+} from "react-icons/fa";
 
 export default function Weather() {
     const API_KEY = "d41ba901f7d73a979c632e4bb500af1d";
@@ -23,6 +29,10 @@ export default function Weather() {
     const [date, setDate] = useState("");
     const [month, setMonth] = useState("");
     const [year, setYear] = useState("");
+
+    const [searchCity, setSearchCity] = useState("toronto");
+    const [hasSubmitted, setHasSubmitted] = useState(false);
+    const [isValid, setIsValid] = useState(true);
 
     function timeConverter(UNIX_timestamp, UTC_timezone) {
         const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -38,27 +48,60 @@ export default function Weather() {
     }
 
     useEffect(() => {
-        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=toronto,ca&units=metric&appid=${API_KEY}`)
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&units=metric&appid=${API_KEY}`)
         .then(res => {
+            console.log(searchCity, res.data);
             setApiResult(res.data);
 
             setWeather(res.data.weather[0]);
             timeConverter(res.data.dt, res.data.timezone);
+
+            setIsValid(true);
+        }).catch(error => {
+            setIsValid(false);
         })
-    }, []);
+    }, [hasSubmitted]);
+
+    useEffect(() => {
+        setIsValid(true);
+    }, [searchCity])
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        setHasSubmitted(!hasSubmitted);
+    }
+
+    function setCity(event) {
+        setSearchCity(event.target.value);
+    }
 
     return (
         apiResult &&
-            <div>
-                <h1>Weather Forecast</h1>
-                <Card>
-                    <Container>
+            <div className="d-flex flex-column align-items-center">
+                <h1 className="mb-4">Weather Forecast</h1>
+                <Card 
+                    className="p-5"
+                    style={{ 
+                        background: "rgba(255, 255, 255, 0.7)", 
+                        color: "" 
+                    }}
+                >
+                    {isValid || <Alert variant="danger">Cannot find {searchCity} City</Alert>}
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Control 
+                            type="text" 
+                            placeholder={"Search by City Name"}
+                            onChange={setCity}
+                            style={{ borderRadius: "100px", opacity: "80%"}}
+                        />
+                    </Form>
+                    <Container className="p-3" /* style={{ border: "1px solid", "border-radius": "25px" }} */>
                         <Row>
-                            <div className="d-flex justify-content-center align-items-center">
+                            <div className="d-flex flex-column justify-content-center align-items-center">
                                 <h3>{day}</h3>
-                                <div style={{ "font-size": "10px" }}>
-                                    <div>{date} {month} {year}</div>
-                                    <div><FaMapMarkerAlt />{apiResult.name}, {apiResult.sys.country}</div>
+                                <div style={{ "fontSize": "10px" }}>
+                                    <div>{date} {month} {year} <FaMapMarkerAlt /> {apiResult.name}, {apiResult.sys.country}</div>
                                 </div>
                             </div>
                         </Row>
@@ -66,11 +109,11 @@ export default function Weather() {
                         <Row className="align-items-center">
                             <Col className="d-flex flex-column align-items-center">
                                 <Image src={`https://openweathermap.org/img/wn/${weather.icon}@2x.png`}></Image>
-                                <h3>{weather.main}</h3>
+                                {/* <h3>{weather.main}</h3> */}
                             </Col>
                             <Col>
                                 <h1>{apiResult.main.temp}°C</h1>
-                                <div>Feels like {apiResult.main.feels_like}</div>
+                                <div>Feels like {apiResult.main.feels_like}°C</div>
                             </Col>
                         </Row>
                     </Container>
@@ -78,11 +121,11 @@ export default function Weather() {
                         <Row>
                             <Col>
                                 <div>Min: {apiResult.main.temp_min}°C</div>
-                                <div>Max: {apiResult.main.temp_max}°C</div>
+                                <div>Humidity: {apiResult.main.humidity}%</div>
                             </Col>
                             <Col>
-                                <div>Humidity: {apiResult.main.humidity}%</div>
-                                <div>Wind: {apiResult.wind.speed}/s</div>
+                                <div>Max: {apiResult.main.temp_max}°C</div>
+                                <div>Wind: {apiResult.wind.speed} m/s</div>
                             </Col>
                         </Row>
                     </Container>
